ffda46778b1e8cb2d37d21b3ea32b8e3b5cc689b
Daniel Grabczewski <danielgrabczewski@gmail.com>
2025-02-04 14:16:33 +1300

    Fix missing imports in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import "./style.scss";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
-
+  const { currentUser } = useContext(AuthContext);
+  
   const ProtectedRoute = ({ children }) => {
     if (!currentUser) {
       return <Navigate to="/login" />;
