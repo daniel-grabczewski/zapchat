4b506f66458974962ca6e4ae4ef608a491f7753d
Daniel Grabczewski <danielgrabczewski@gmail.com>
2025-02-04 14:29:34 +1300

    Add ChatContext and provider setup

---
+++ src/context/ChatContext.jsx
@@ -0,0 +1,33 @@
+import {
+  createContext,
+  useReducer,
+} from "react";
+
+export const ChatContext = createContext();
+
+export const ChatContextProvider = ({ children }) => {
+  const INITIAL_STATE = {
+    chatId: "null",
+    user: {},
+  };
+
+  const chatReducer = (state, action) => {
+    switch (action.type) {
+      case "CHANGE_USER":
+        return {
+          user: action.payload,
+          chatId: "",
+        };
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(chatReducer, INITIAL_STATE);
+
+  return (
+    <ChatContext.Provider value={{ data: state, dispatch }}>
+      {children}
+    </ChatContext.Provider>
+  );
+};
