07a0510707c853f2ac3cc2b1a82af08c95bf612e
Daniel Grabczewski <danielgrabczewski@gmail.com>
2025-02-05 18:15:08 +1300

    Allow user to upload avatar image via Firebase Storage

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react"
-import { createUserWithEmailAndPassword } from "firebase/auth"
-import { auth } from "../firebase"
-import { Link, useNavigate } from "react-router-dom"
-import Add from "../img/addAvatar.png"
+import React, { useState } from 'react'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '../firebase'
+import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
+import { storage } from '../firebase'
+import { Link, useNavigate } from 'react-router-dom'
+import Add from '../img/addAvatar.png'
 
 const Register = () => {
   const [err, setErr] = useState(false)
@@ -15,12 +17,23 @@ const Register = () => {
     const displayName = e.target[0].value
     const email = e.target[1].value
     const password = e.target[2].value
+    const file = e.target[3].files[0]
 
     try {
       // Create user with email and password
       const res = await createUserWithEmailAndPassword(auth, email, password)
-      console.log("User created:", res.user)
-      setLoading(false)
+
+      // Create a unique image name
+      const date = new Date().getTime()
+      const storageRef = ref(storage, `${displayName + date}`)
+
+      // Upload the file
+      await uploadBytesResumable(storageRef, file).then(() => {
+        getDownloadURL(storageRef).then((downloadURL) => {
+          console.log('File available at', downloadURL)
+          setLoading(false)
+        })
+      })
     } catch (err) {
       console.log(err)
       setErr(true)
@@ -37,13 +50,13 @@ const Register = () => {
           <input required type="text" placeholder="display name" />
           <input required type="email" placeholder="email" />
           <input required type="password" placeholder="password" />
-          <input required style={{ display: "none" }} type="file" id="file" />
+          <input required style={{ display: 'none' }} type="file" id="file" />
           <label htmlFor="file">
             <img src={Add} alt="" />
             <span>Add an avatar</span>
           </label>
           <button disabled={loading}>Sign up</button>
-          {loading && "Loading..."}
+          {loading && 'Uploading...'}
           {err && <span>Something went wrong</span>}
         </form>
         <p>
