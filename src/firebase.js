e4e8d6a0855ac2fb79590b9bff8f0a67e2e48fdc
Daniel Grabczewski <danielgrabczewski@gmail.com>
2025-02-06 13:04:52 +1300

    Transition firebase.js to use .env to get config details

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,19 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getStorage } from 'firebase/storage'
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
-};
+  apiKey: process.env.ZAP_CHAT_FIREBASE_API_KEY,
+  authDomain: process.env.ZAP_CHAT_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.ZAP_CHAT_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.ZAP_CHAT_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.ZAP_CHAT_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.ZAP_CHAT_FIREBASE_APP_ID,
+  measurementId: process.env.ZAP_CHAT_FIREBASE_MEASUREMENT_ID,
+}
 
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth();
-export const storage = getStorage();
+export const app = initializeApp(firebaseConfig)
+export const auth = getAuth()
+export const storage = getStorage()
 export const db = getFirestore()
