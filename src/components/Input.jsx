8c2a45d6e66c96ceee0440be90e81af5c63df3d4
Daniel Grabczewski <danielgrabczewski@gmail.com>
2025-02-10 11:50:58 +1300

    Integrate Firebase for sending text messages and updating user chats

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,6 +3,15 @@ import Img from '../img/img.png'
 import Attach from '../img/attach.png'
 import { AuthContext } from '../context/AuthContext'
 import { ChatContext } from '../context/ChatContext'
+import {
+  arrayUnion,
+  doc,
+  serverTimestamp,
+  Timestamp,
+  updateDoc,
+} from 'firebase/firestore'
+import { db } from '../firebase'
+import { v4 as uuid } from 'uuid'
 
 const Input = () => {
   const [text, setText] = useState('')
@@ -12,7 +21,30 @@ const Input = () => {
   const { data } = useContext(ChatContext)
 
   const handleSend = async () => {
-    // TODO: implement send functionality
+    // If no image is attached, send a text-only message
+    if (!img) {
+      await updateDoc(doc(db, 'chats', data.chatId), {
+        messages: arrayUnion({
+          id: uuid(),
+          text,
+          senderId: currentUser.uid,
+          date: Timestamp.now(),
+        }),
+      })
+    }
+    // Update the last message and timestamp for the current user
+    await updateDoc(doc(db, 'userChats', currentUser.uid), {
+      [data.chatId + '.lastMessage']: { text },
+      [data.chatId + '.date']: serverTimestamp(),
+    })
+    // Update the last message and timestamp for the recipient
+    await updateDoc(doc(db, 'userChats', data.user.uid), {
+      [data.chatId + '.lastMessage']: { text },
+      [data.chatId + '.date']: serverTimestamp(),
+    })
+
+    setText('')
+    setImg(null)
   }
   return (
     <div className="input">
