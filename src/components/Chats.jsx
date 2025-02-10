7cf148fe171cb57e82bc409b0a9fb565b10825c6
Daniel Grabczewski <danielgrabczewski@gmail.com>
2025-02-11 10:15:00 +1300

    Add Firebase integration to fetch chats using onSnapshot

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,7 +1,26 @@
-import React, { useState } from "react"
+import { doc, onSnapshot } from "firebase/firestore"
+import React, { useContext, useEffect, useState } from "react"
+import { AuthContext } from "../context/AuthContext"
+import { db } from "../firebase"
 
 const Chats = () => {
   const [chats, setChats] = useState([])
+
+  const { currentUser } = useContext(AuthContext)
+
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data())
+      })
+
+      return () => {
+        unsub()
+      }
+    }
+
+    currentUser.uid && getChats()
+  }, [currentUser.uid])
 
   return (
     <div className="chats">
