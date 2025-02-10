b9bf8117d991db5abe588e0d2b2a95bdaf31e96e
Daniel Grabczewski <danielgrabczewski@gmail.com>
2025-02-11 10:18:00 +1300

    Finalize Chats component with full UI and ChatContext integration

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,16 +1,18 @@
-import { doc, onSnapshot } from "firebase/firestore"
-import React, { useContext, useEffect, useState } from "react"
-import { AuthContext } from "../context/AuthContext"
-import { db } from "../firebase"
+import { doc, onSnapshot } from 'firebase/firestore'
+import React, { useContext, useEffect, useState } from 'react'
+import { AuthContext } from '../context/AuthContext'
+import { ChatContext } from '../context/ChatContext'
+import { db } from '../firebase'
 
 const Chats = () => {
   const [chats, setChats] = useState([])
 
   const { currentUser } = useContext(AuthContext)
+  const { dispatch } = useContext(ChatContext)
 
   useEffect(() => {
     const getChats = () => {
-      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+      const unsub = onSnapshot(doc(db, 'userChats', currentUser.uid), (doc) => {
         setChats(doc.data())
       })
 
@@ -22,9 +24,27 @@ const Chats = () => {
     currentUser.uid && getChats()
   }, [currentUser.uid])
 
+  const handleSelect = (u) => {
+    dispatch({ type: 'CHANGE_USER', payload: u })
+  }
+
   return (
     <div className="chats">
-      <p>Chats will be displayed here.</p>
+      {Object.entries(chats)
+        ?.sort((a, b) => b[1].date - a[1].date)
+        .map((chat) => (
+          <div
+            className="userChat"
+            key={chat[0]}
+            onClick={() => handleSelect(chat[1].userInfo)}
+          >
+            <img src={chat[1].userInfo.photoURL} alt="" />
+            <div className="userChatInfo">
+              <span>{chat[1].userInfo.displayName}</span>
+              <p>{chat[1].lastMessage?.text}</p>
+            </div>
+          </div>
+        ))}
     </div>
   )
 }
